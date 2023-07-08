2a944840ee47aa8bc864fd698517712f79a532f8
Dialla23 <diallomahamoudou39@yahoo.fr>
2023-07-08 12:06:55 -0300

    Initial commit and index.html and index.js created

---
+++ index.js
@@ -0,0 +1,41 @@
+let start_quiz_btn= document.getElementById('start_quiz')
+let quiz_question= document.getElementById('quiz_question')
+let initial_division= document.getElementById('initial_division')
+let quizTime= 25;
+let timer_section= document.getElementById('timer_section')
+let questions= [{
+    name:"question 1",
+    choises:["choise 1","choise 2", "choise 3"],
+    answers: "choise 2"
+},{
+    name:"question 2",
+    choises:["choise 1","choise 2", "choise 3"],
+    answers: "choise 1"
+},{
+    name:"question 3",
+    choises:["choise 1","choise 2", "choise 3"],
+    answers: "choise 3"
+}]
+
+start_quiz_btn.addEventListener('click',function(event){
+    event.preventDefault()
+
+    initial_division.style.display= "none"
+    startQuiz()
+})
+
+function startQuiz(){
+    startTimer()
+}
+
+function startTimer(){
+    let timer= setInterval(function(){
+        if(quizTime >=0){
+            let timerText=`Time left : ${quizTime}`
+            quizTime--
+            timer_section.innerHTML=timerText
+        }else{
+            clearInterval(timer)
+        }
+    },1000)
+}
